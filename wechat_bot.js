dcff40b95285ae7b0c9a7ef4e7deaed89b9a5522
jonchurch <me@jonchurch.com>
2017-05-25 01:38:48 -0400

    respond to 'hi' using customer service api

--- wechat_bot.js
+++ wechat_bot.js
@@ -3,6 +3,7 @@
 var Botkit = require('./lib/Botkit.js')
 
 var controller = Botkit.wechatbot({
+    debug: true,
     access_token: process.env.access_token,
     app_secret: process.env.app_secret,
 
@@ -10,11 +11,12 @@ var controller = Botkit.wechatbot({
 
 var bot = controller.spawn({})
 
-controller.setupWebserver(80, function(err, webserver) {
+controller.setupWebserver(process.env.port, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
     console.log('Online!')})
 })
 
 controller.hears('hi', 'message_received', function(bot, message) {
+    console.log('=====HEARD HI WOWEEE!')
     bot.reply(message, 'Ni hao!')
 })
